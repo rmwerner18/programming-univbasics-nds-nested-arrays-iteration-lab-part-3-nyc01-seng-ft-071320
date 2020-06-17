f0a018fedc224ce24fcb81290134e92efc7a5dbf
Ryan Werner <rmwerner@umich.edu>
2020-06-17 17:00:02 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     inner_count = 0
     while inner_count < src[outer_count].length do
       if src[outer_count][inner_count].is_a? String
-        joined_phrase += src[outer_count][inner_count]
+        joined_phrase += src[outer_count][inner_count] + " "
       end
       inner_count += 1
     end
